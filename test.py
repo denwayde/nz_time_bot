511f45fc296455fe951f9cb5811a51babb9bb68f
denwayde <denwayde09@gmail.com>
2023-03-16 15:40:20 +0500

    data visualisation

---
+++ test.py
@@ -0,0 +1,7 @@
+import requests
+import json
+
+resp = requests.get(
+    "http://api.aladhan.com/v1/calendarByCity/2023/3?city=Ufa&country=Russia&method=14&school=1")
+
+print(json.dumps(resp.json()['data'][22]))
