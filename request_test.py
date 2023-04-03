57e8e1aafd4c866a8e30104ebdc10bbc81e6ed51
denwayde <denwayde09@gmail.com>
2023-04-03 11:00:16 +0500

    преобразовали дату из апи в нужную. теперь нужно прописывать логику бота.

--- request_test.py
+++ request_test.py
@@ -11,14 +11,19 @@ resp = requests.get(
 tz_moscow = pytz.timezone("Europe/Moscow")
 
 # resp.json()['data'][21]['timings'] vremenena nz
-# resp.json()['data'][21]['date']['gregorian']['date'] data kotoruy nuzno preobrazovat v nuznii format pered sohraneniem v bd
+# resp.json()['data'][21]['date']['gregorian']['date'] data kotoruy nuzno preobrazovat v nuznii format pered tem kak vstavlst v bd
 # resp.json()['data'][21]['date']['gregorian']['month']['number'] nomer mesyatca
 # resp.json()['data'][21]['date']['gregorian']['year']
 # resp.json()['data'][21]['meta']['timezone'] timezonechik
+# resp.json()['data'][21]['date']['hijri']['holidays']
+dt_str = resp.json()['data'][21]['date']['gregorian']['date']
+# preobrazovannaya data которую будем интегрировать в БД
+dt_str_conv = dt.strptime(dt_str, '%d-%m-%Y')
+# print(dt_str_conv.date() == dt.now().date())
 
 
 timings = resp.json()['data'][21]['timings']
-print(timings)
+# print(timings)
 tmings_arr = []
 delta_time = dt.now(tz_moscow).hour-dt.now().hour
 
@@ -33,4 +38,4 @@ for x in timings:
     tmings_arr.append(d.strftime('%H:%M'))
 
 # СДЕЛАЛИ ВРЕМЕНА ДЛЯ ТАСКОВ СКОРЕЕ ВСЕГО ОБЪЕДИНЯТЬ ИХ В МАССИВ НЕ ОБЯЗАТЕЛЬНО
-print(tmings_arr)
+# print(tmings_arr)
